769a6349c429bf94f20e28ae11edcc2c45cfb1b5
Service Web <sw@gssmail.com>
2024-03-15 09:03:20 -0500

    Commit changes in C:\Websites\Git\emarlow\5907\GCG_5907_Header.sql

---
+++ GCG_5907_Header.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "GCG_5907_Header"(
+ "UserID" CHAR(8),
+ "Job" CHAR(6),
+ "Suffix" CHAR(3),
+ "JS" CHAR(10),
+ "Part" CHAR(20),
+ "Location" CHAR(2),
+ "Part17" CHAR(17),
+ "Rev" CHAR(3),
+ "Qty_Order" NUMERIC(12,4),
+ "Released" VARCHAR(50),
+ "Date_Start" DATE,
+ "Date_Due" DATE,
+ "CanKit" CHAR(1) );
